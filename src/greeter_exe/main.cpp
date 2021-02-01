0c7073f5c1ed0be2be29a45eedc74753b770cde6
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2021-01-31 20:03:46 -0500

    bencmarks and main

--- src/greeter_exe/main.cpp
+++ src/greeter_exe/main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include "greeter_impl.hpp"
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
+
+ABSL_FLAG(std::string, name, "Margot", "The person to be greeted");
 
 int main(int argc, char** argv)
 {
-    std::cout << "greeter" << std::endl;
+    absl::SetProgramUsageMessage("I'll say hello to anyone you want");
+    absl::ParseCommandLine(argc, argv);
+    const auto name = absl::GetFlag(FLAGS_name);
+
+    greeter::Greeter g;
+    g.greet(name);
 
     return 0;
 }
---
+++ src/greeter_lib/benchmarks/greeter_benchmarks.cpp
@@ -0,0 +1,34 @@
+#include "benchmark/benchmark.h"
+
+#include "greeter.h"
+#include "greeter_impl.hpp"
+
+#define TEST_NAME "Margot"
+
+using greeter::Greeter;
+
+void GreeterNative(benchmark::State& state)
+{
+    Greeter g;
+
+    for (auto _ : state)
+    {
+        g.greet(TEST_NAME);
+    }
+}
+
+BENCHMARK(GreeterNative);
+
+void GreeterLib(benchmark::State& state)
+{
+    auto* g = greeter_new();
+
+    for (auto _ : state)
+    {
+        greeter_greet(g, TEST_NAME);
+    }
+
+    greeter_free(g);
+}
+
+BENCHMARK(GreeterLib);
